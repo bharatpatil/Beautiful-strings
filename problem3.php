e752d7c3c14f137993d4c11718bf24c3400a9530
Bharat Patil <bharatpatil@Bharats-MacBook-Pro.local>
2013-01-28 15:36:34 +0530

    problem3

--- problem3.php
+++ problem3.php
@@ -1,36 +1,79 @@
 <?php
+function nextVal($arr,$start)
+{
+    $j = $start;
+    $l = count($arr);
+    $ret = $start - 1;
+    while ($j < $l and $arr[$j])
+    {
+        $ret = $j;
+        $j += 1;
+    }
+    return $ret;
+	
+}
 
 $fpWrite = fopen('problem3Output.txt','w+');
-$fp = fopen('problem3Input.txt','r');
+$fp = fopen('find_the_mintxt.txt','r');
 
 $counter = (int)fgets($fp);
 $countCase = 1;
 while(!feof($fp) && $counter--)
 {
 	$line1 = fgets($fp); 
-	$n = explode(' ',$line1);
-	$k = $n[1];
-	$n = $n[0];
-	
+	$e = explode(' ',$line1);
+	$k = (int)$e[1];
+	$n = (int)$e[0];
 	$line2 = fgets($fp);
 	$e = explode(' ',$line2);
-	$a = $e[0];
-	$b = $e[1];
-	$c = $e[2];
-	$r = $e[3];
-	
+	$a = (int)$e[0];
+	$b = (int)$e[1];
+	$c = (int)$e[2];
+	$r = (int)$e[3];
+	$m = array_fill(0,4*$k,0);
+	$s = array_fill(0,$k+1,0);
 
-	$m = array();
-	array_push($m,$a);
-	for($i=1;$i<$k;$i++)
+	$m[0] = $a;
+	if ($m[0] <= $k)
+		$s[$m[0]] = 1;
+	foreach(range(1,$k) as $i)
 	{
-		array_push($m,($b * $m[$i-1] + $c) % $r);
-		echo $i .' = '. $m[$i] . "\n";
-	}
+		$m[$i] = ($b * $m[$i-1] + $c) % $r;
+		if($m[$i] < $k+1)
+		    $s[$m[$i]] += 1;
+	}        
 	
-	sort($m);	
-	print_r($m);	
-	exit;
+	$p = nextVal($s,0);
+
+	$m[$k] = $p + 1;
+	$p = nextVal($s, $p+2);
+	
+	
+	foreach(range($k+1,$n) as $i)
+	{
+		if($m[$i-$k-1] > $p ||  $s[$m[$i-$k-1]] > 1)
+		{
+		    $m[$i] = $p + 1;
+		    if ($m[$i-$k-1] <= $k)
+		        $s[$m[$i-$k-1]] -= 1;
+		    $s[$m[$i]] += 1;
+		    $p = nextVal($s, $p+2);
+		}
+		else
+		{
+		    $m[$i] = $m[$i-$k-1];
+		}
+		if ($p == $k)
+		    break;
+	}
+
+	$outputLine = '';
+	if ($p != $k)
+        	$outputLine = 'Case #'.$countCase++.': '.$m[$n-1]."\n";
+	else
+		$outputLine = 'Case #'.$countCase++.': '.$m[$i-$k + ($n-$i+$k+$k) % ($k+1)]."\n"; 
+	echo $outputLine;	
+	fwrite($fpWrite,$outputLine);	
 }
 
 fclose($fp);
